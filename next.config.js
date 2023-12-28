c362800f6d40680f34ed9884d31eb44607871015
Leulseged Bekele <88849889+Leulbkm@users.noreply.github.com>
2023-12-28 22:40:48 +0300

    fix: change and delete unecessary data

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'source.unsplash.com',
+            port: '',
+            pathname: '/600x600/**',
+          },
+        ],
+      },
+}
 
 module.exports = nextConfig
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
-import type { Config } from 'tailwindcss'
+const withMT = require("@material-tailwind/react/utils/withMT");
+import type { Config } from 'tailwindcss';
 
-const config: Config = {
+const daisyuiConfig: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,11 +11,39 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        backGroundGreen: '#44979C'
       },
     },
   },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: false,
+    darkTheme: "bright",
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    logs: true,
+    themeRoot: ":root",
+  },
+};
+
+const mtConfig = withMT({
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-}
-export default config
+});
+
+module.exports = {
+  ...mtConfig,
+  ...daisyuiConfig,
+};
