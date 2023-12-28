76c9c0e7ba96529c55f281a37a868ccd9968852d
Leulseged Bekele <88849889+Leulbkm@users.noreply.github.com>
2023-12-28 22:53:38 +0300

    feat: use signup component

---
+++ app/signup/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+import React from 'react';
+import SignupForm from '../components/SignUpLogin/SignupForm';
+const Home: React.FC = () => {
+  const handleSubmit = async (credentials: { username: string; email: string; password: string; repassword: string}) => {
+    // Implement your login submission logic here
+    console.log('Login submitted:', credentials);
+  };
+
+  return (
+    <>
+      <SignupForm
+        onSubmit={handleSubmit}
+        error="Error message goes here" // Replace with actual error state
+        alreadyHaveAcc={true} // Replace with actual state
+        toSignUp={() => console.log('Navigate to SignUp')} // Replace with actual navigation logic
+        toLogIn={() => console.log('Navigate to LogIn')} // Replace with actual navigation logic
+      />
+    </>
+  );
+};
+
+export default Home;
