efe7085366c170850efb835ae924c0040548befc
Leulseged Bekele <88849889+Leulbkm@users.noreply.github.com>
2024-02-23 20:11:40 +0300

    config url of image and backend

--- next.config.js
+++ next.config.js
@@ -1,15 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'source.unsplash.com',
-            port: '',
-            pathname: '/600x600/**',
-          },
-        ],
+// next.config.js
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:4000/api/:path*', // Replace with your backend URL
       },
+    ];
+  },
+};
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/600x600/**',
+      },
+    ],
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
